e6a601cf9fc4fbc774b1a49b6c708db48c2af0aa
HojiakbarSultonov <hojiakbarrasulovich@gmail.com>
2023-06-21 21:41:19 +0500

    start mini project

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,24 +6,24 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function index(){
-        return 'Bu userlarni royxati';
-    }
-
-    public function show( $user){
-
-        return view('users.show', ['name'=>"Hojiakbar"]);
-    }
-
-    public function create(){
-        return view('users.create');
-    }
-
-    public function store(Request $request){
-        dd($request->input('name'));
-    }
-
-    public function edit($user_id){
-        return $user_id . 'ni o\'zgartirish';
-    }
+//    public function index(){
+//        return 'Bu userlarni royxati';
+//    }
+//
+//    public function show( $user){
+//
+//        return view('users.show', ['name'=>"Hojiakbar"]);
+//    }
+//
+//    public function create(){
+//        return view('users.create');
+//    }
+//
+//    public function store(Request $request){
+//        dd($request->input('name'));
+//    }
+//
+//    public function edit($user_id){
+//        return $user_id . 'ni o\'zgartirish';
+//    }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Controller;
-use App\Http\Controllers\UserController;
+//use App\Http\Controllers\UserController;
+use App\Http\Controllers\PageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,15 +16,13 @@ use App\Http\Controllers\UserController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/users',[UserController::class, 'index']);
-Route::get('/users/create',[UserController::class, 'create']);
-Route::get('/users/{user}',[UserController::class, 'show']);
-Route::get('/users/{user}/edit',[UserController::class, 'edit']);
-Route::post('/user-create', [UserController::class, 'store']);
+Route::get('/', [PageController::class, 'main']);
+//
+//Route::get('/users',[UserController::class, 'index']);
+//Route::get('/users/create',[UserController::class, 'create']);
+//Route::get('/users/{user}',[UserController::class, 'show']);
+//Route::get('/users/{user}/edit',[UserController::class, 'edit']);
+//Route::post('/user-create', [UserController::class, 'store']);
 
 
 
