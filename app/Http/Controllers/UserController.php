718eda08cc880fa386a6c6b0ccf4219b2edeb754
HojiakbarSultonov <hojiakbarrasulovich@gmail.com>
2023-06-21 19:42:42 +0500

    views

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index(){
+        return 'Bu userlarni royxati';
+    }
+    public function show($user){
+        return view('users.show', ['name'=>"Hojiakbar"]);
+    }
+    public function create(){
+        return view('users.create');
+    }
+    public function edit($user_id){
+        return $user_id . 'ni o\'zgartirish';
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\UserController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/users',[UserController::class, 'index']);
+Route::get('/users/create',[UserController::class, 'create']);
+Route::get('/users/{user}',[UserController::class, 'show']);
+Route::get('/users/{user}/edit',[UserController::class, 'edit']);
+
+
+
+
